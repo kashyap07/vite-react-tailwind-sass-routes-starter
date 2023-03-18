6fa97643ce307890b431b4e17fbfb3504ab51a35
Suhas Kashyap <kashyapsuhas07@gmail.com>
2023-03-18 16:54:21 +0530

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import clsx from "clsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main className="flex items-center justify-center flex-col h-screen w-screen gap-5">
+      <h1 className="text-3xl">Vite kitchen sink starter</h1>
+      <h2 className="text-xl">
+        Vite + React + TS + Tailwind + Sass + Prettier + Generouted
+      </h2>
+    </main>
+  );
 }
 
-export default App
+export default App;
