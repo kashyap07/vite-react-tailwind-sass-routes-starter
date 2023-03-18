4bc8ff21e6495cfcdac46e1b870d83b6bcf3a27b
Suhas Kashyap <kashyapsuhas07@gmail.com>
2023-03-18 17:05:25 +0530

    add custom tw config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import clsx from "clsx";
-
 function App() {
   return (
-    <main className="flex items-center justify-center flex-col h-screen w-screen gap-5">
+    <main className="flex h-screen w-screen flex-col items-center justify-center gap-5">
       <h1 className="text-3xl">Vite kitchen sink starter</h1>
       <h2 className="text-xl">
         Vite + React + TS + Tailwind + Sass + Prettier + Generouted
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        200: "2",
+        250: "2.5",
+      },
+      zIndex: {
+        "-10": "-10",
+      },
+    },
   },
-  plugins: [],
+
+  plugins: [
+    plugin(function ({ addComponents }) {
+      const miscUtils = {
+        ".position-unset": {
+          position: "unset",
+        },
+      };
+      addComponents(miscUtils);
+    }),
+
+    plugin(function ({ addVariant }) {
+      addVariant("children-iterative", "& *");
+      addVariant("children", "& > *");
+      addVariant("supports-grid", "@supports (display: grid)");
+    }),
+  ],
 };
